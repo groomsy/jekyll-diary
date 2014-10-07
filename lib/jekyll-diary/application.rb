1d2cd9c6fb14eced8b80fc4fd86ef52e8daa1596
Todd Grooms <todd.grooms@gmail.com>
2014-10-07 16:41:18 -0500

    Adding Ability to Publish a Draft

--- lib/jekyll-diary/application.rb
+++ lib/jekyll-diary/application.rb
@@ -43,9 +43,30 @@ module JekyllDiary
           run do |opts, args|
             did_understand_command = true
             if args.length == 0
-              puts "You must specify a draft to publish."
+              puts "You must specify a draft to publish. Either give the path to the draft or just the filename."
             else
-              puts "You want to publish #{args.first}."
+              draft_file_path = nil
+              if !File.exists?(args.first)
+                if !File.exists?("_drafts/#{args.first}")
+                  puts "Invalid draft specified! #{args.first} was not found."
+                else
+                  draft_file_path = "_drafts/#{args.first}"
+                end
+              else
+                draft_file_path = args.first
+              end
+
+              unless draft_file_path.nil?
+                draft_file_path_components = draft_file_path.split('/')
+                draft_file_name = draft_file_path_components.last
+                published_file_name = "#{Time.now.strftime("%Y-%m-%d")}-#{draft_file_name}"
+                published_file_path = "_posts/#{published_file_name}"
+              
+                puts "Publishing #{draft_file_path}\nDraft File Name: #{draft_file_name}\nPublished File Name: #{published_file_name}"
+                File.rename(draft_file_path, published_file_path)
+                `git add --all`
+                `git commit -m 'Publishing #{draft_file_name}'`
+              end
             end
           end
         end
